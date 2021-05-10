123a721fb1d7f3a245ff9ee7cc3fa868c926dce5
Matheus José <matheus.jose@i9xp.com.br>
2021-05-10 18:28:44 -0300

    register ok

--- src/components/ClientScreen/ClientRegister.vue
+++ src/components/ClientScreen/ClientRegister.vue
@@ -8,13 +8,22 @@
           </div>
         </q-card-section>
         <q-list>
-          <q-form @submit="onSubmit" class="form" ref="userCreateForm">
+          <q-form @submit="onSubmit" class="form" ref="clientCreateForm">
             <q-card-section>
-              <ClientManagement :password-rules="passwordRules" :confirm-password-rules="confirmPasswordRules" :editItem="editItem"></ClientManagement>
+              <ClientManagement :editItem="editItem"></ClientManagement>
             </q-card-section>
             <div class="" align="right">
-              <q-btn @click="cancelAdd" class="q-ma-md" label="Cancelar"></q-btn>
-              <q-btn label="Salvar" type="submit" class="q-ma-md" color="primary"/>
+              <q-btn
+                @click="cancelAdd"
+                class="q-ma-md"
+                label="Cancelar"
+              ></q-btn>
+              <q-btn
+                label="Salvar"
+                type="submit"
+                class="q-ma-md"
+                color="primary"
+              />
             </div>
           </q-form>
         </q-list>
@@ -24,98 +33,88 @@
 </template>
 
 <script>
-import ClientManagement from "./clientManagement.vue"
-import { defineComponent } from '@vue/composition-api'
+import ClientManagement from "./clientManagement.vue";
+import { defineComponent } from "@vue/composition-api";
 
 const defaultItem = {
-  name: '' ,
-  email: '' ,
-  password: '',
-  confirmPassword: '',
-  group_id: ''
-}
+  name: "",
+  email: "",
+  cnpj: "",
+  empresa: "",
+  departamento: "",
+  telefone: ""
+};
 
 export default defineComponent({
   name: "ClientRegister",
   components: { ClientManagement },
-  data () {
+  data() {
     return {
       editItem: defaultItem,
-      prompt: false,
-      passwordRules: [
-        v => !!v || 'Senha obrigatória',
-        v => v.length >= 6 || 'Minimo 6 caracteres'
-      ],
-      confirmPasswordRules: [
-        v => !!v || 'Senha obrigatória',
-        v => v.length >= 6 || 'Minimo 6 caracteres'
-      ]
-    }
+      prompt: false
+    };
   },
 
   methods: {
     onSubmit() {
-
-      this.editItem.group_id = this.editItem.group.value;
-
       this.$swal({
-        title: 'Cadastrar Usuário ?',
-        icon: 'warning',
+        title: "Cadastrar Cliente ?",
+        icon: "warning",
         showCancelButton: true,
-        confirmButtonText: 'Cadastrar',
-        cancelButtonText: 'Cancelar',
+        confirmButtonText: "Cadastrar",
+        cancelButtonText: "Cancelar",
         showLoaderOnConfirm: true
-      }).then((result) => {
-
+      }).then(result => {
         if (result.isConfirmed) {
-
-          this.$axios.post('users', this.editItem).then(response => {
-            this.$swal({
-              title: response.data.Message,
-              icon: 'success',
-              showConfirmButton: true
+          this.$axios
+            .post("clients", this.editItem)
+            .then(response => {
+              this.$swal({
+                title: response.data.Message,
+                icon: "success",
+                showConfirmButton: true
+              });
+              this.resetForm();
+              this.$emit("showCreate", false);
             })
-            this.resetForm();
-            this.$emit('showCreate', false);
-          })
-          .catch(error => {
-            this.$swal({
-              title: 'Erro ao cadastrar !',
-              text: 'Entre em contato com o suporte',
-              icon: 'error',
-              showConfirmButton: true
-            })
-          });
+            .catch(error => {
+              this.$swal({
+                title: "Erro ao cadastrar !",
+                text: "Entre em contato com o suporte",
+                icon: "error",
+                showConfirmButton: true
+              });
+            });
         }
-      })
+      });
     },
 
-    cancelAdd () {
+    cancelAdd() {
       this.$swal({
-        title: 'Cancelar cadastro ?',
-        icon: 'warning',
+        title: "Cancelar cadastro ?",
+        icon: "warning",
         showCancelButton: true,
-        cancelButtonText: 'Voltar',
-        confirmButtonText: 'Cancelar'
-      }).then((result) => {
+        cancelButtonText: "Voltar",
+        confirmButtonText: "Cancelar"
+      }).then(result => {
         if (result.isConfirmed) {
           this.resetForm();
-          this.$emit('showCreate', false);
+          this.$emit("showCreate", false);
         }
-      })
+      });
     },
 
-    resetForm () {
-      defaultItem.name = '';
-      defaultItem.email = '';
-      defaultItem.password = '';
-      defaultItem.confirmPassword = '';
-      defaultItem.group_id = '';
+    resetForm() {
+      defaultItem.name = "";
+      defaultItem.email = "";
+      defaultItem.password = "";
+      defaultItem.confirmPassword = "";
+      defaultItem.group_id = "";
     },
 
-    hide(){
-      this.prompt = false
+    hide() {
+      this.prompt = false;
     }
   }
-})
+});
 </script>
--- src/components/ClientScreen/clientManagement.vue
+++ src/components/ClientScreen/clientManagement.vue
@@ -2,144 +2,136 @@
   <div>
     <q-item>
       <q-item-section>
-        <q-input outlined v-model="editItem.name" type="text" label="Nome completo" lazy-rules :rules="[ v => !!v || 'Nome obrigatório']"/>
+        <q-input
+          outlined
+          v-model="editItem.name"
+          type="text"
+          label="Nome completo"
+          lazy-rules
+          :rules="[v => !!v || 'Nome obrigatório']"
+        />
       </q-item-section>
     </q-item>
     <q-item items-center>
       <q-item-section>
-        <q-input outlined v-model="editItem.email" label="Email" type="email" lazy-rules
-                 :rules="emailRules" />
+        <q-input
+          outlined
+          v-model="editItem.email"
+          label="Email"
+          type="email"
+          lazy-rules
+          :rules="emailRules"
+        />
       </q-item-section>
     </q-item>
-    <q-item>
+    <q-item items-center>
       <q-item-section>
-        <q-select outlined  v-model="editItem.group" :options="groups" label="Grupo" />
+        <q-input
+          outlined
+          v-model="editItem.cnpj"
+          label="Cnpj"
+          type="text"
+          lazy-rules
+          :rules="cnpjRules"
+        />
       </q-item-section>
     </q-item>
-    <q-item class="col-md-12 justify-center">
-      <div class="col-md-5">
-        <q-input bottom-slots v-model="editItem.password" name="password" id="password" :type="visibilityPass" label="Senha" class="q-ma-sm" lazy-rules
-                 :rules="passwordRules">
-          <template v-slot:prepend>
-            <q-icon name="vpn_key" />
-          </template>
-          <template v-slot:after>
-            <!-- shows and hides the password -->
-            <q-btn flat @click="showPassword()" v-if="visibilityPass == 'password'" color="black" icon="fas fa-eye" />
-            <q-btn flat @click="hidePassword()" v-if="visibilityPass == 'text'" color="black" icon="fas fa-eye-slash" />
-          </template>
-        </q-input>
-      </div>
-      <div class="col-md-5">
-        <q-input v-model="editItem.confirmPassword" name="confirmPassword" id="confirmPassword" :type="visibilityConfirmPass" label="Confirmar Senha" class="q-ma-sm" lazy-rules
-                 :rules="confirmPasswordRules">
-          <template v-slot:prepend>
-            <q-icon name="vpn_key" />
-          </template>
-          <template v-slot:after>
-            <!-- shows and hides the password -->
-            <q-btn flat @click="showConfirmPassword()" v-if="visibilityConfirmPass == 'password'" color="black" icon="fas fa-eye" />
-            <q-btn flat @click="hideConfirmPassword()" v-if="visibilityConfirmPass == 'text'" color="black" icon="fas fa-eye-slash" />
-          </template>
-        </q-input>
-      </div>
+    <q-item items-center>
+      <q-item-section>
+        <q-input
+          outlined
+          v-model="editItem.company"
+          label="Empresa"
+          type="text"
+          lazy-rules
+          :rules="companyRules"
+        />
+      </q-item-section>
+    </q-item>
+    <q-item items-center>
+      <q-item-section>
+        <q-input
+          outlined
+          v-model="editItem.department"
+          label="Departamento"
+          type="text"
+          lazy-rules
+          :rules="departmentRules"
+        />
+      </q-item-section>
+    </q-item>
+    <q-item items-center>
+      <q-item-section>
+        <q-input
+          outlined
+          v-model="editItem.phone"
+          label="Empresa"
+          type="text"
+          lazy-rules
+          :rules="phoneRules"
+        />
+      </q-item-section>
     </q-item>
   </div>
 </template>
 
 <script>
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent } from "@vue/composition-api";
 const defaultItem = {
-  name: '',
-  email: '',
-  password: '',
-  confirmPassword: '',
-  group_id: ''
-}
-
-function wrapCsvValue(val, formatFn) {
-  let formatted = formatFn !== void 0 ? formatFn(val) : val;
-
-  formatted =
-    formatted === void 0 || formatted === null ? "" : String(formatted);
-
-  formatted = formatted.split('"').join('""');
-
-  return `"${formatted}"`;
-}
+  name: "",
+  email: "",
+  cnpj: "",
+  company: "",
+  department: "",
+  phone: ""
+};
 
 export default defineComponent({
   props: {
-    show_form: Boolean,
-    editItem: {},
-    passwordRules: null,
-    confirmPasswordRules: null
+    editItem: {}
   },
-  name: 'clientManagement',
-  data () {
+  name: "clientManagement",
+  data() {
     return {
       emailRules: [
-        v => !!v || 'Email é obrigatório',
-        v => /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()\\[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v) || 'Email Inválido'
+        v => !!v || "Email é obrigatório",
+        v =>
+          /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()\\[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+            v
+          ) || "Email Inválido"
       ],
-      password: '',
-      confirmPassword: '',
-      visibilityPass: 'password',
-      visibilityConfirmPass: 'password',
-      groups: [],
-      group: ''
-    }
+      cnpjRules: [v => !!v || "Cnpj é obrigatório"],
+      companyRules: [v => !!v || "Empresa é obrigatório"],
+      departmentRules: [v => !!v || "Departamento é obrigatório"],
+      phoneRules: [v => !!v || "Telefone é obrigatório"]
+    };
   },
-  mounted () {
-    this.confirmPasswordRules.push(v => v === this.editItem.password || 'A senha é diferente, repita a senha')
-    if (typeof this.$axios.defaults.headers.common.Authorization === 'undefined' || this.$axios.defaults.headers.common.Authorization === '') {
-      this.$router.push({ path: '/' })
+  mounted() {
+    if (
+      typeof this.$axios.defaults.headers.common.Authorization ===
+        "undefined" ||
+      this.$axios.defaults.headers.common.Authorization === ""
+    ) {
+      this.$router.push({ path: "/" });
     }
-
-    this.$axios.get('/group').then((response) => {
-      response.data.forEach((item) => {
-        this.groups.push({value: item._id, label: item.group})
-      })
-    }).catch(() => {
-      this.$swal({
-        title: 'Erro ao buscar grupos!',
-        text: 'Entre em contato com o suporte',
-        icon: 'error',
-        showConfirmButton: true
-      })
-    })
   },
   methods: {
-
-    showPassword() {
-      this.visibilityPass = 'text';
-    },
-    hidePassword() {
-      this.visibilityPass = 'password';
-    },
-
-    showConfirmPassword() {
-      this.visibilityConfirmPass = 'text';
-    },
-    hideConfirmPassword() {
-      this.visibilityConfirmPass = 'password';
-    },
-
-    close (){
+    close() {
       setTimeout(() => {
-        this.editItem = Object.assign({}, this.defaultItem)
+        this.editItem = Object.assign({}, this.defaultItem);
         this.resetForm();
-        this.editedIndex = -1
-        this.$emit('hide_create_user', { show: false, reset: false });
-      }, 300)
+        this.editedIndex = -1;
+        this.$emit("hide_create_client", { show: false, reset: false });
+      }, 300);
     },
 
-    resetForm () {
-      defaultItem.name = '';
-      defaultItem.email = '';
-      defaultItem.password = '';
-      defaultItem.confirmPassword = '';
-      defaultItem.group_id.value = '';
+    resetForm() {
+      defaultItem.name = "";
+      defaultItem.email = "";
+      defaultItem.cnpj = "";
+      defaultItem.company = "";
+      defaultItem.department = "";
+      defaultItem.phone = "";
     }
   }
 });
